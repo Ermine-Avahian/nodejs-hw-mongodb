5d7245b774fc006001ad37a28ad5db782b03ff8f
Ermine <avahian.hem@gmail.com>
2025-03-17 15:05:11 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-const boot = async () => {
+const bootstrap  = async () => {
   await initMongoConnection();
   setupServer();
 };
 
-boot();
+bootstrap ();
