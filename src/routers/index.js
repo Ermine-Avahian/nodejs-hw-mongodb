8764c5dadc402697469e881ddd1a3aae609d884d
Ermine <avahian.hem@gmail.com>
2025-03-27 13:14:09 +0100

    fix

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,11 +4,6 @@ import authRouter from './auth.js';
 
 const router = Router();
 
-router.use((req, res, next) => {
-  console.log(`Request made to: ${req.method} ${req.url}`);
-  next();
-});
-
 router.use('/contacts', contactsRouter);
 
 router.use('/auth', authRouter);
