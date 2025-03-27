cc7dbc5dfe6b66f95625cc523e7c458e65f548a2
Ermine <avahian.hem@gmail.com>
2025-03-27 12:19:27 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -29,6 +29,10 @@ export const setupServer = () => {
 
   app.use('/uploads', express.static(UPLOAD_DIR));
 
+  app.get('/', (req, res) => {
+    res.json({ message: 'API is running! Use /auth or /contacts' });
+  });
+
   app.use(notFoundHandler);
 
   app.use(errorHandler);
